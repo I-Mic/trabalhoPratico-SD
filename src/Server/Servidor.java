d54c7b4e2787601115ce21bea2eecb496e0ee861
I-Mic <61886573+I-Mic@users.noreply.github.com>
2022-01-15 17:20:20 +0000

    ta mesmo quase

--- src/Server/Servidor.java
+++ src/Server/Servidor.java
@@ -14,8 +14,24 @@ public class Servidor {
         ServerSocket ss = new ServerSocket(12345);
         ServidorFacade sf = new ServidorFacade();
 
+        sf.setUtilizadores(UtilizadoresDAO.getInstanceObj("input_files/Utilizadores"));
+        sf.setReservas(ReservasDAO.getInstanceObj("input_files/Reservas"));
+        //sf.setVoos(VoosDao.getInstanceObj("input_files/Voos"));
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                System.out.println("Prepare to exit");
+                UtilizadoresDAO.saveInstanceObj(sf.getUtilizadores(),"input_files/Utilizadores");
+                ReservasDAO.saveInstanceObj(sf.getReservas(),"input_files/Reservas");
+                //VoosDao.saveInstanceObj(sf.getVoos(),"input_files/Voos");
+            }
+        });
+
+
+
         while(true){
             Socket s = ss.accept();
+
             ServerWorker sw=new ServerWorker(s,sf);
             new Thread(sw).start();
         }
